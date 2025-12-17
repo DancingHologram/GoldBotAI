2456e85315721aee7d5f71f1fac80949bb99d8d2
Hologram <cngolden3@gmail.com>
2025-12-17 04:05:44 -0800

    Added call_function

--- main.py
+++ main.py
@@ -4,10 +4,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.run_python_file import schema_run_python_file
-from functions.write_file import schema_write_file
+from call_function import call_function, available_functions
 
 
 def main():
@@ -24,10 +21,6 @@ def main():
     args = parser.parse_args()
 
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-
-    available_functions = types.Tool(
-        function_declarations=[schema_get_files_info, schema_get_file_content, schema_run_python_file, schema_write_file],
-    )
 
     generated_content = client.models.generate_content(
         model="gemini-2.5-flash",
@@ -48,8 +41,14 @@ def main():
     if generated_content.function_calls is None:
         print(generated_content.text)
     else:
+        function_responses = []
         for f in generated_content.function_calls:
-            print(f"Calling function: {f.name}({f.args})")
+            function_call_results = call_function(f, verbose=args.verbose)
+            if not function_call_results.parts or not function_call_results.parts[0].function_response:
+                raise Exception ("empty function call result")
+            if args.verbose:
+                print(f"-> {function_call_results.parts[0].function_response.response}")
+            function_responses.append(function_call_results.parts[0])
 
 
 if __name__ == "__main__":
